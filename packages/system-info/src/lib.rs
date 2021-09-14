b93cb7b9cd5f6d85c46d9ef991f8733061d02385
mojahige <mojahige@gmail.com>
2021-09-15 02:34:47 +0900

    🪄 feat: add CPU speed to return

--- packages/system-info/src/lib.rs
+++ packages/system-info/src/lib.rs
@@ -1,16 +1,34 @@
 use neon::prelude::*;
 
-fn get_cpu_num(mut cx: FunctionContext) -> JsResult<JsNumber> {
-    let result: f64 = match sys_info::cpu_num() {
-        Ok(num) => num as f64,
-        Err(_) => 0 as f64,
-    };
+fn get_cpu_num() -> u32 {
+    match sys_info::cpu_num() {
+        Ok(num) => num,
+        Err(_) => 0 as u32,
+    }
+}
 
-    Ok(cx.number(result))
+fn get_cpu_speed() -> u64 {
+    match sys_info::cpu_speed() {
+        Ok(speed) => speed,
+        Err(_) => 0,
+    }
+}
+
+fn get_system_info(mut cx: FunctionContext) -> JsResult<JsObject> {
+    let info = cx.empty_object();
+    let cpu = cx.empty_object();
+    let cpu_quantity = cx.number(get_cpu_num() as f64);
+    let cpu_speed = cx.number(get_cpu_speed() as f64);
+
+    cpu.set(&mut cx, "quantity", cpu_quantity)?;
+    cpu.set(&mut cx, "speed", cpu_speed)?;
+    info.set(&mut cx, "cpu", cpu)?;
+
+    Ok(info)
 }
 
 #[neon::main]
 fn main(mut cx: ModuleContext) -> NeonResult<()> {
-    cx.export_function("get", get_cpu_num)?;
+    cx.export_function("get", get_system_info)?;
     Ok(())
 }
