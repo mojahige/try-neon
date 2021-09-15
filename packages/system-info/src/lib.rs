1b38649d3b4a91eb19c9af6cd5731b1e3d220a4d
mojahige <mojahige@gmail.com>
2021-09-15 09:32:51 +0900

    🪄 feat: add OS type to return

--- packages/system-info/src/lib.rs
+++ packages/system-info/src/lib.rs
@@ -14,15 +14,28 @@ fn get_cpu_speed() -> u64 {
     }
 }
 
+fn get_os_type() -> String {
+    match sys_info::os_type() {
+        Ok(os_type) => os_type,
+        Err(_) => String::from("")
+    }
+}
+
 fn get_system_info(mut cx: FunctionContext) -> JsResult<JsObject> {
     let info = cx.empty_object();
     let cpu = cx.empty_object();
     let cpu_quantity = cx.number(get_cpu_num() as f64);
     let cpu_speed = cx.number(get_cpu_speed() as f64);
+    let os = cx.empty_object();
+    let os_type = cx.string(get_os_type());
 
     cpu.set(&mut cx, "quantity", cpu_quantity)?;
     cpu.set(&mut cx, "speed", cpu_speed)?;
+
+    os.set(&mut cx, "type", os_type)?;
+
     info.set(&mut cx, "cpu", cpu)?;
+    info.set(&mut cx, "os", os)?;
 
     Ok(info)
 }
